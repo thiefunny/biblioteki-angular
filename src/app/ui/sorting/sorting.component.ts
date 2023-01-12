34712242dcb44a0119f1118150d8e76c63908ee4
thiefunny <mikolaj@walanus.com>
2023-01-12 13:22:07 +0100

    sortinf and filtering in separate components

--- src/app/ui/sorting/sorting.component.ts
+++ src/app/ui/sorting/sorting.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { SortingService } from './sorting.service';
 
 @Component({
   selector: 'app-sorting',
   templateUrl: './sorting.component.html',
-  styleUrls: ['./sorting.component.scss']
+  styleUrls: ['./sorting.component.scss'],
 })
 export class SortingComponent {
-
+  constructor(protected sortingService: SortingService) {}
 }
